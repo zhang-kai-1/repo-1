dd4b329a4d44753cac1d15ae2aa3cc4f43ba7d07
zhang-kai-1 <17629257404@163.com>
2019-10-17 12:22:29 +0800

    bbb.java ccc.java

--- src/aaa/bbb.java
+++ src/aaa/bbb.java
@@ -3,5 +3,6 @@ package aaa;
 public class bbb {
 public static void main(String[] args) {
 	System.out.println("ccccccccc");
+	System.out.println("1111111");
 }
 }
--- src/aaa/ccc.java
+++ src/aaa/ccc.java
@@ -4,6 +4,7 @@ public class ccc {
 private void syso() {
 	System.out.println("cccccccccc");
 	syso();
+	System.out.println("dddddddd");
 
 }
 }
