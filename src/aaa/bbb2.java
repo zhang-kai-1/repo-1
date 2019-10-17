17122ee827af0921460c3424550e7adfd64e1442
zhang-kai-1 <17629257404@163.com>
2019-10-17 12:30:14 +0800

    bbb2.java

--- src/aaa/bbb2.java
+++ src/aaa/bbb2.java
@@ -2,8 +2,6 @@ package aaa;
 
 public class bbb2 {
 public static void main(String[] args) {
-	System.out.println("ccccccccc");
-	System.out.println("1111111");
-	System.out.println("1111111");
+	
 }
 }
